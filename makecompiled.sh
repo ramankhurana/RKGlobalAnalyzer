937746a0e047a0e339bbd020896d47d1f4f584dd
Raman Khurana <raman.khurana@cern.ch>
2015-07-05 12:17:42 -0500

    fixed total events

--- makecompiled.sh
+++ makecompiled.sh
@@ -6,7 +6,7 @@ g++ `root-config --cflags` RKMonoHAnalyzer/src/CutFlowAndEachCut.C  -c
 g++ `root-config --cflags` RKMonoHAnalyzer/src/NMinusOne.C  -c 
 g++ `root-config --cflags` RKMonoHAnalyzer/src/HistFactory.C  -c 
 g++ `root-config --cflags` RKMonoHAnalyzer/src/ABCDMethod.C  -c 
-g++ `root-config --cflags` RKAnalysisController/src/Main.C RKAnalyzer.o JetValidator.o METValidator.o SelectionBitsProducer.o CutFlowAndEachCut.o NMinusOne.o HistFactory.o ABCDMethod.o -o main.exe `root-config --libs`
+#g++ `root-config --cflags` RKAnalysisController/src/Main.C RKAnalyzer.o JetValidator.o METValidator.o SelectionBitsProducer.o CutFlowAndEachCut.o NMinusOne.o HistFactory.o ABCDMethod.o -o main.exe `root-config --libs`
 ## This can be used instead of previous line in order to submit using farmout analysis
-#g++ `root-config --cflags` RKAnalysisController/src/MyfarmoutAnalyzer.cc RKAnalyzer.o JetValidator.o METValidator.o SelectionBitsProducer.o CutFlowAndEachCut.o NMinusOne.o HistFactory.o ABCDMethod.o   -o main.exe `root-config --libs`
+g++ `root-config --cflags` RKAnalysisController/src/MyfarmoutAnalyzer.cc RKAnalyzer.o JetValidator.o METValidator.o SelectionBitsProducer.o CutFlowAndEachCut.o NMinusOne.o HistFactory.o ABCDMethod.o   -o main.exe `root-config --libs`
 
